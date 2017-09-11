6ef825da22523e49fc0246cc093de8b9a1655394
Hui.Li <hui.li@hypereal.com>
2017-09-11 13:55:49 +0800

    Add some comments to example.cpp

--- example.cpp
+++ example.cpp
@@ -14,8 +14,10 @@ public:
 
 int main(int argc, char *argv[])
 {
+    // Construct a devMgr for the current devices
     DriveEnum::DeviceManager devMgr(DriveEnum::eCurrent);
 
+    // Add properties we want, device does not have every device.
     devMgr.AddProperty(DriveEnum::eClassGUID);
     devMgr.AddProperty(DriveEnum::eEnumeratorName);
     devMgr.AddProperty(DriveEnum::eDescription);
@@ -28,11 +30,13 @@ int main(int argc, char *argv[])
     devMgr.AddProperty(DriveEnum::eLocationInformation);
     devMgr.AddProperty(DriveEnum::eLocationPaths);
 
+    //Processing...
     Progress prog;
     devMgr.Process(&prog);
 
     unsigned int devCount = devMgr.Count();
 
+    //Output device
     for (unsigned int i = 0; i < devCount; i++)
     {
         DriveEnum::Device device = *devMgr.GetDevice(i);
@@ -45,6 +49,7 @@ int main(int argc, char *argv[])
         std::wcout << "Path:" << device.propertyAsWString(DriveEnum::eLocationPaths) << std::endl;
      }
 
+    // Constuct the properteis we want to sort - this is what driver needs
     std::vector<DriveEnum::Property> slots;
     slots.push_back(DriveEnum::eClassGUID);
     slots.push_back(DriveEnum::ePublicInf0);
@@ -52,9 +57,16 @@ int main(int argc, char *argv[])
     slots.push_back(DriveEnum::eMFG);
     slots.push_back(DriveEnum::eEnumeratorName);
 
+    // Construct the DeviceSort with the correct data structure 
+    // GUID : ClassGUID
+    // PublicInf0 : std::wstring
+    // OriginalInf0 : std::wstring
+    // MFG(Provider) : std::wstring
+    // EnumeratorName : std::wstring
     DriveEnum::DeviceSort<GUID, std::wstring, std::wstring, std::wstring, std::wstring> sort(devMgr, slots);
     sort.sorting();
 
+    // Check out how many device types
     slots.clear();
     slots.push_back(DriveEnum::eEnumeratorName);
     DriveEnum::DeviceSort<std::wstring> devType(devMgr, slots);
